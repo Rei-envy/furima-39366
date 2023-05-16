fc31cdcd04ec644fe8f63d7dac379fa2f0421dfd
Rei <reiacewakaba310@gmail.com>
2023-05-16 18:43:31 +0900

    itemモデルの単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,15 +13,15 @@ class Item < ApplicationRecord
     validates :name,         presence: true
     validates :introduce,    presence: true
 
-    #半角数字のみを許可し、300~9999999までの範囲を許可
-    with_options presence: true, format: { with: /\A[0-9]+\z/i } do
-        validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999}
-    end
-
     #ジャンルの選択が「---」の時は保存できないようにする
     validates :category_id, numericality: { other_than: 1 }
     validates :statement_id, numericality: { other_than: 1 }
     validates :load_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :send_day_id, numericality: { other_than: 1 }
+
+    #半角数字のみを許可し、300~9999999までの範囲を許可
+    with_options presence: true, format: { with: /\A[0-9]+\z/i } do
+        validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999}
+    end
 end
