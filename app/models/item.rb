258e6ebaa08d83430d807a72211d2e8312e3b3e3
Rei <reiacewakaba310@gmail.com>
2023-05-16 15:25:08 +0900

    ActiveHashの導入と各モデルの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :load
+    belongs_to :prefecture
+    belongs_to :send_day
+    belongs_to :statement
 end
