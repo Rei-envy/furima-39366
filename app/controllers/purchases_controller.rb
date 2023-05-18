9c3c685a0f8f51ea1a7be2133bc3a9761c6cedb4
Rei <reiacewakaba310@gmail.com>
2023-05-18 15:15:15 +0900

    Formオブジェクトなどの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,5 +2,26 @@ class PurchasesController < ApplicationController
 
     def index
         @purchase_address = PurchaseAddress.new
+        @purchase = Purchase.new(purchase_params)
+    end
+
+    def create
+        @purchase = Purchase.create(purchase_params)
+        @purchase_address = PurchaseAddress.new(purchase_address_params)
+        if @purchase_address.valid?
+            @purchase_address.save
+            redirect_to root_path
+        else
+            render :index
+        end
+    end
+
+    private
+    def purchase_params
+        params.permit(:name, :image, :price, :load_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    def purchase_address_params
+        params.require(:purchase_address).permit(:post_code, :prefecture_id, :locality, :house_number, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id],purchase_id: @purchase.id)
     end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,18 +1,16 @@
 class PurchaseAddress
     include ActiveModel::Model
-    attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :locality, :house_number, :building, :phone
-
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :prefecture
+    attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :locality, :house_number, :building, :phone, :purchase_id
 
     # ここにバリデーションの処理を書く
     with_options presence: true do
         validates :user_id
         validates :item_id
-        validates :post_code       format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+        validates :post_code,       format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
         validates :locality
         validates :house_number    
-        validates :phone           format: {with: /\A[0-9]{10,11}\z/}
+        validates :phone,           format: {with: /\A[0-9]{10,11}\z/}
+        validates :purchase_id
     end
     # ジャンルの選択が「---」の時は保存できないようにする
     validates :prefecture_id, numericality: { other_than: 1 }
@@ -21,6 +19,6 @@ class PurchaseAddress
         # 購入情報を保存し、変数purchaseに代入する
         purchase = Purchase.create(user_id: user_id, item_id: item_id)
         # 住所を保存する
-        Address.create(post_code: post_code, presence_id: prefecture_id, locality: locality, house_number: house_number, building: building, phone: phone, purchase_id: purchase.id)
+        Address.create(post_code: post_code, prefecture_id: prefecture_id, locality: locality, house_number: house_number, building: building, phone: phone, purchase_id: purchase.id)
     end
 end
